6533da3fff41974017e4a6a64a82ca979eaab618
Ryu <ryu@realtime.tw>
2022-01-14 17:26:22 +0800

    first commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,25 +3,28 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/airbnb',
-  ],
+  extends: ["plugin:vue/vue3-essential", "@vue/airbnb"],
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: "babel-eslint",
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
   overrides: [
     {
       files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)",
+        "*.vue",
+        "*.js",
       ],
       env: {
         jest: true,
+      },
+      rules: {
+        "max-len": "off", // 關閉一行最長幾個字
+        quotes: [0, "double"],
       },
     },
   ],
