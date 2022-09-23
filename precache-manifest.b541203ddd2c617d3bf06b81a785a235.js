c28966972f6d0f6c0c8d83081700152e9a382eab
Ryu <ryu@realtime.tw>
2022-09-24 00:42:06 +0800

    deploy

---
+++ precache-manifest.b541203ddd2c617d3bf06b81a785a235.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64d8c7bce9e47d0946e4a51c7b3e2e80",
+    "url": "assets/img/header.png"
+  },
+  {
+    "revision": "48025182349af4abfe3e272a7e128c4c",
+    "url": "assets/img/introduction-music.png"
+  },
+  {
+    "revision": "90840bfca190b45f7cd04ed9d8c2cd54",
+    "url": "assets/img/introduction-users.png"
+  },
+  {
+    "revision": "eba639c1b0a9287dd3a1904b9ae9831f",
+    "url": "assets/img/profile-pic.jpg"
+  },
+  {
+    "revision": "d899ba7d2cac3f11c4377a700d68d22e",
+    "url": "assets/img/song-header.png"
+  },
+  {
+    "revision": "e7b9aeb02c86a922404d13073d21836a",
+    "url": "assets/img/user-header.png"
+  },
+  {
+    "revision": "e1e8a906fbadaaed8064",
+    "url": "css/app.113be1a3.css"
+  },
+  {
+    "revision": "82788a0e5be3e4d2db63",
+    "url": "css/chunk-537381ea.a9f56982.css"
+  },
+  {
+    "revision": "2dc145fd6dc144b242c6",
+    "url": "css/chunk-vendors.06a7122d.css"
+  },
+  {
+    "revision": "1baa10f80d8b0637ae68375d9562e73d",
+    "url": "index.html"
+  },
+  {
+    "revision": "e1e8a906fbadaaed8064",
+    "url": "js/app.adcf88bb.js"
+  },
+  {
+    "revision": "40b3ba3c7c01151ee9f5",
+    "url": "js/chunk-2d21a3d2.0997b729.js"
+  },
+  {
+    "revision": "82788a0e5be3e4d2db63",
+    "url": "js/chunk-537381ea.f963c0f5.js"
+  },
+  {
+    "revision": "24c569885e53a5d2cf1d",
+    "url": "js/chunk-5bc818be.cfa89a83.js"
+  },
+  {
+    "revision": "2dc145fd6dc144b242c6",
+    "url": "js/chunk-vendors.50e7be27.js"
+  },
+  {
+    "revision": "7d670ed8db3464554c42",
+    "url": "js/groupedChunk.f39f3559.js"
+  },
+  {
+    "revision": "686f9c495bcdca671c2a003ac6c3ddb1",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
